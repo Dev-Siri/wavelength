a1cc355185c8b2a4f4b76bc852c859fa6738ff4b
Dev-Siri <s0431668@gmail.com>
2025-01-14 22:08:46 +0530

    Set trustHost: true for auth.js

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -4,6 +4,7 @@ import Google from "@auth/sveltekit/providers/google";
 import { PRIVATE_GOOGLE_CLIENT_ID, PRIVATE_GOOGLE_CLIENT_SECRET } from "$env/static/private";
 
 export const { handle } = SvelteKitAuth({
+  trustHost: true,
   providers: [
     Google({ clientId: PRIVATE_GOOGLE_CLIENT_ID, clientSecret: PRIVATE_GOOGLE_CLIENT_SECRET }),
   ],
