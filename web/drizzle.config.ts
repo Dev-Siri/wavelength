62ded81126fbc4028effd47ebbb76eafce8aca25
Dev-Siri <s0431668@gmail.com>
2025-01-16 21:49:14 +0530

    Set verbose & strict in drizzle to true

--- web/drizzle.config.ts
+++ web/drizzle.config.ts
@@ -7,6 +7,8 @@ export default defineConfig({
   schema: "./src/lib/db/schema.ts",
   dialect: "postgresql",
   out: "./drizzle",
+  verbose: true,
+  strict: true,
   dbCredentials: {
     url: process.env.PRIVATE_DSN!,
   },
