1f825e33fbff93253dc79b01b1e1a0045dddcd2a
Dev-Siri <s0431668@gmail.com>
2025-01-16 22:03:58 +0530

    temp fix: remove migration lines from drizzle.ts

--- web/src/lib/db/drizzle.ts
+++ web/src/lib/db/drizzle.ts
@@ -1,11 +1,11 @@
 import { PRIVATE_DSN } from "$env/static/private";
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import { migrate } from "drizzle-orm/neon-http/migrator";
+// import { migrate } from "drizzle-orm/neon-http/migrator";
 
 const sql = neon(PRIVATE_DSN);
 const db = drizzle(sql);
 
-await migrate(db, { migrationsFolder: "drizzle" });
+// await migrate(db, { migrationsFolder: "drizzle" });
 
 export default db;
