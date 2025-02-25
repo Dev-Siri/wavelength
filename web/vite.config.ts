f5ddc4b8430efb9e0979f0e4dac3d12a75cb6b49
Dev-Siri <s0431668@gmail.com>
2025-02-25 18:10:10 +0530

    Post dependency update to latest

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,9 @@
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit(), enhancedImages()],
-  build: {
-    target: "esnext",
-  },
+  plugins: [sveltekit(), tailwindcss(), enhancedImages()],
+  build: { target: "esnext" },
 });
