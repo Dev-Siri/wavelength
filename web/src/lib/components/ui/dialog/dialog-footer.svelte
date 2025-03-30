54b1c7666aa110226a3fe46e4f5f48c6da78f83a
Dev-Siri <s0431668@gmail.com>
2025-03-30 21:29:23 +0530

    Add missing dialog portal from shadcn-svelte old source

--- web/src/lib/components/ui/dialog/dialog-footer.svelte
+++ web/src/lib/components/ui/dialog/dialog-footer.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
-	import cn from "$lib/utils/cn";
+  import cn from "$lib/utils/cn";
 
-	import type { WithElementRef } from "bits-ui";
-	import type { HTMLAttributes } from "svelte/elements";
+  import type { WithElementRef } from "bits-ui";
+  import type { HTMLAttributes } from "svelte/elements";
 
-	let {
-		ref = $bindable(null),
-		class: className,
-		children,
-		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
 </script>
 
 <div
-	bind:this={ref}
-	class={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)}
-	{...restProps}
+  bind:this={ref}
+  class={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)}
+  {...restProps}
 >
-	{@render children?.()}
+  {@render children?.()}
 </div>
