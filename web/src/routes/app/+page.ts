eca96edb3d6b84236012510703761d97fd10ea23
Dev-Siri <s0431668@gmail.com>
2025-07-18 22:25:00 +0530

    Add customFetch: fetch, svelte's fetch function, to queryClient on index

--- web/src/routes/app/+page.ts
+++ web/src/routes/app/+page.ts
@@ -4,13 +4,16 @@ import type { ApiResponse } from "$lib/utils/types";
 import { DEFAULT_REGION } from "$lib/constants/countries";
 import queryClient from "$lib/utils/query-client";
 
-export async function load({ url }) {
-  const regionResponse = await queryClient<ApiResponse<string>>(url.toString(), "/api/region");
+export async function load({ url, fetch }) {
+  const regionResponse = await queryClient<ApiResponse<string>>(url.toString(), "/api/region", {
+    customFetch: fetch,
+  });
 
   const response = queryClient<ApiResponse<QuickPicksResponse["results"]>>(
     url.toString(),
     "/api/music/quick-picks",
     {
+      customFetch: fetch,
       searchParams: {
         regionCode: regionResponse.success ? regionResponse.data : DEFAULT_REGION,
       },
