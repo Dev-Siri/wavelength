c3d9ff5eba85f95bb8ce434b4a389e21b1731704
Dev-Siri <s0431668@gmail.com>
2025-08-29 16:13:34 +0530

    Undo all req forgery changes in sveltekit

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -5,9 +5,6 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    csrf: {
-      checkOrigin: false,
-    },
     adapter: adapter(),
   },
 };
