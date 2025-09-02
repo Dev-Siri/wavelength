43b61c6c2abc9fe423198edc39ef85b079f5dc61
Dev-Siri <s0431668@gmail.com>
2025-09-02 23:37:15 +0530

    Add manual image uploads for mobile app as an api route

--- web/src/lib/constants/urls.ts
+++ web/src/lib/constants/urls.ts
@@ -2,6 +2,7 @@ import { dev } from "$app/environment";
 
 export const RAPID_API_URL = "https://youtube-music-api3.p.rapidapi.com";
 export const LYRICS_RAPID_API_URL = "https://spotify-scraper.p.rapidapi.com";
+export const UPLOADTHING_API_URL = "https://api.uploadthing.com";
 export const YT_IMG_API_URL = "https://img.youtube.com";
 export const BASE_URL = dev ? "http://localhost:5173" : "https://mavelength.vercel.app";
 export const YOUTUBE_HEADER_IMAGE_START_URL = "yt3.ggpht.com/";
