549efa0d939a7a65c423b6c5758797dd86ce5a27
Dev-Siri <s0431668@gmail.com>
2025-09-21 21:30:56 +0530

    Change default port to 8080

--- server/env/port.go
+++ server/env/port.go
@@ -9,8 +9,8 @@ func GetPORT() string {
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		go logging.Logger.Warn("No PORT environment variable found, defaulting to 3000")
-		return "5000"
+		go logging.Logger.Warn("No PORT environment variable found, defaulting to 8080")
+		return "8080"
 	}
 
 	return port
