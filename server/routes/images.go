a0d00c26765ab45f2cc3b535eb9ec510b32c631a
Dev-Siri <s0431668@gmail.com>
2025-09-26 00:04:30 +0530

    Delete go server's image optimizer route to shift it back to the web app with serverless

--- server/routes/images.go
+++ server/routes/images.go
@@ -7,7 +7,6 @@ import (
 )
 
 func registerImageRoutes(app *fiber.App) {
-	app.Get("/image", image_controllers.GetOptimizedImage)
 	app.Get("/image/theme-color", image_controllers.GetThemeColor)
 	app.Post("/image/manual-upload", image_controllers.ManualImageUpload)
 }
