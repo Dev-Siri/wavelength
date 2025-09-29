4c1fbe863352afeeb77e2d18b65b3ad1c50c0adc
Dev-Siri <s0431668@gmail.com>
2025-09-30 01:13:56 +0530

    Fix geoip parsing struct shape

--- server/models/geoip_data.go
+++ server/models/geoip_data.go
@@ -1,13 +1,13 @@
 package models
 
 type GeoIpData struct {
-	Range    [2]int `json:"range"`
-	Country  string `json:"country"`
-	Region   string `json:"region"`
-	Eu       string `json:"eu"`
-	Timezone string `json:"timezone"`
-	City     string `json:"city"`
-	Ll       [2]int `json:"ll"`
-	Metro    int    `json:"metro"`
-	Area     int    `json:"area"`
+	Range    [2]float64 `json:"range"`
+	Country  string     `json:"country"`
+	Region   string     `json:"region"`
+	Eu       string     `json:"eu"`
+	Timezone string     `json:"timezone"`
+	City     string     `json:"city"`
+	Ll       [2]float64 `json:"ll"`
+	Metro    int        `json:"metro"`
+	Area     int        `json:"area"`
 }
