956441eda15cdb486e2535228470e1b1382deaf2
Dev-Siri <s0431668@gmail.com>
2025-10-02 03:40:34 +0530

    Remove audio url route

--- server/routes/streams.go
+++ server/routes/streams.go
@@ -7,7 +7,6 @@ import (
 )
 
 func registerStreamRoutes(app *fiber.App) {
-	app.Get("/stream/url/:videoId/audio", stream_controllers.GetAudioUrl)
 	app.Get("/stream/playback/:videoId/audio", stream_controllers.GetAudioStream)
 	app.Get("/stream/playback/:videoId/video", stream_controllers.GetVideoStream)
 }
