c48454c5ed6b31b6e7703666f4b43adcd88f16a1
Dev-Siri <s0431668@gmail.com>
2025-10-02 05:22:27 +0530

    Add video streaming changes to controller

--- server/controllers/streams/get_video_stream.go
+++ server/controllers/streams/get_video_stream.go
@@ -1,7 +1,7 @@
 package stream_controllers
 
 import (
-	"bytes"
+	"io"
 	"os/exec"
 	"wavelength/constants"
 	"wavelength/env"
@@ -18,25 +18,48 @@ func GetVideoStream(ctx *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "Video ID is required.")
 	}
 
+	rangeHeader := ctx.Get("Range")
+	playbackRange, err := utils.ParseRangeHeader(rangeHeader)
+
+	if err != nil {
+		return err
+	}
+
 	cmd := exec.Command(
 		ytDlpPath,
 		"-f", constants.SupportedVideoStreamingFormat,
-		"-g", utils.GetYouTubeWatchUrl(videoId),
 		"--cookies", env.GetYtCookiePath(),
+		"--no-check-certificate",
+		"-o", "-",
+		utils.GetYouTubeWatchUrl(videoId),
 	)
 
-	var stderr bytes.Buffer
-
-	cmd.Stderr = &stderr
-
-	out, err := cmd.Output()
-
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, "yt-dlp failed: "+err.Error()+" - "+stderr.String())
+		return fiber.NewError(fiber.StatusInternalServerError, "Failed to pipe: "+err.Error())
 	}
 
-	ytDlpOutput := string(out)
-	videoURL := ytDlpOutput[:len(ytDlpOutput)-1]
+	if err := cmd.Start(); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "yt-dlp start error: "+err.Error())
+	}
 
-	return ctx.Redirect(videoURL, 302)
+	if playbackRange != nil {
+		return handleRangeRequest(ctx, stdout, cmd, playbackRange.Start, playbackRange.End)
+	}
+
+	ctx.Set("Content-Type", "video/mp4")
+	ctx.Set("Accept-Ranges", "bytes")
+
+	_, err = io.Copy(ctx.Response().BodyWriter(), stdout)
+
+	if err != nil {
+		cmd.Process.Kill()
+		return fiber.NewError(fiber.StatusInternalServerError, "stream copy error: "+err.Error())
+	}
+
+	if err := cmd.Wait(); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "yt-dlp failed: "+err.Error())
+	}
+
+	return nil
 }
