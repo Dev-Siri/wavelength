530b826c0be0634416eeeab904824f4132660e90
Dev-Siri <s0431668@gmail.com>
2025-10-13 02:01:54 +0530

    Fix optional query parsing

--- server/controllers/playlist/edit_playlist.go
+++ server/controllers/playlist/edit_playlist.go
@@ -21,12 +21,43 @@ func EditPlaylist(ctx *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "Invalid JSON body: "+err.Error())
 	}
 
-	_, err := db.Database.Exec(`
-		UPDATE playlists
-		SET name = $1, cover_image = $2
-		WHERE playlist_id = $3
-	`, playlistEditBody.Name, playlistEditBody.CoverImage, playlistId)
+	// Build dynamic update query
+	setClauses := []string{}
+	args := []string{}
+	argPos := 1
 
+	if playlistEditBody.Name != "" {
+		setClauses = append(setClauses, "name = $"+string(rune(argPos)))
+		args = append(args, playlistEditBody.Name)
+		argPos++
+	}
+
+	if playlistEditBody.CoverImage != "" {
+		setClauses = append(setClauses, "cover_image = $"+string(rune(argPos)))
+		args = append(args, playlistEditBody.CoverImage)
+		argPos++
+	}
+
+	if len(setClauses) == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "No fields provided for update.")
+	}
+
+	query := "UPDATE playlists SET " +
+		func() string {
+			s := ""
+			for i, c := range setClauses {
+				if i > 0 {
+					s += ", "
+				}
+				s += c
+			}
+			return s
+		}() +
+		" WHERE playlist_id = $" + string(rune(argPos))
+
+	args = append(args, playlistId)
+
+	_, err := db.Database.Exec(query, args)
 	if err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, "Failed to edit playlist: "+err.Error())
 	}
--- server/models/schemas/playlist_edit_schema.go
+++ server/models/schemas/playlist_edit_schema.go
@@ -1,6 +1,6 @@
 package schemas
 
 type PlaylistEditSchema struct {
-	Name       string  `json:"name"`
-	CoverImage *string `json:"coverImage"`
+	Name       string `json:"name"`
+	CoverImage string `json:"coverImage"`
 }
