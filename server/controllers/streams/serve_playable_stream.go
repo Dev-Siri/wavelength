2beeaacf99d57a54c642dd0e5447308f89fafe47
Dev-Siri <s0431668@gmail.com>
2025-11-05 03:19:29 +0530

    Change serve playable stream implementation to use os.Open()

--- server/controllers/streams/serve_playable_stream.go
+++ server/controllers/streams/serve_playable_stream.go
@@ -2,62 +2,67 @@ package stream_controllers
 
 import (
 	"fmt"
+	"io"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func servePlayableStream(ctx *fiber.Ctx, streamPath string, contentType string) error {
-	streamBytes, err := os.ReadFile(streamPath)
-	streamSize := len(streamBytes)
-
+	file, err := os.Open(streamPath)
 	if err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, "Unable to open stream file: "+err.Error())
 	}
 
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Unable to stat stream file: "+err.Error())
+	}
+
+	fileSize := stat.Size()
+	ctx.Set(fiber.HeaderContentType, contentType)
+	ctx.Set(fiber.HeaderAcceptRanges, "bytes")
+	ctx.Set(fiber.HeaderConnection, "keep-alive")
+
 	rangeHeader := ctx.Get("Range")
-
 	if rangeHeader == "" {
-		ctx.Set("Content-Length", fmt.Sprintf("%d", streamSize))
-		ctx.Set("Content-Type", contentType)
-
-		return ctx.Send(streamBytes)
+		ctx.Set(fiber.HeaderContentLength, fmt.Sprintf("%d", fileSize))
+		_, err = io.Copy(ctx.Response().BodyWriter(), file)
+		return err
 	}
 
-	var start, end int
-	var s string
+	var start, end int64
 
-	if _, err := fmt.Sscanf(rangeHeader, "bytes=%s", &s); err != nil {
-		return fiber.NewError(fiber.StatusBadRequest, "Invalid Range header.")
+	_, err = fmt.Sscanf(rangeHeader, "bytes=%d-%d", &start, &end)
+	if err != nil {
+		if _, err := fmt.Sscanf(rangeHeader, "bytes=%d-", &start); err != nil {
+			return fiber.NewError(fiber.StatusBadRequest, "Invalid Range header.")
+		}
+		end = fileSize - 1
 	}
 
-	parts := strings.Split(s, "-")
-
-	if len(parts) != 2 {
-		return fiber.NewError(fiber.StatusBadRequest, "Invalid Range header format.")
+	if end == 0 || end >= fileSize {
+		end = fileSize - 1
 	}
 
-	start, _ = strconv.Atoi(parts[0])
-
-	if parts[1] == "" {
-		end = streamSize - 1
-	} else {
-		end, _ = strconv.Atoi(parts[1])
-	}
-
-	if start > end || end >= streamSize {
+	if start > end || start >= fileSize {
 		ctx.Status(fiber.StatusRequestedRangeNotSatisfiable)
-		ctx.Set("Content-Range", fmt.Sprintf("bytes */%d", streamSize))
+		ctx.Set(fiber.HeaderContentRange, fmt.Sprintf("bytes */%d", fileSize))
 		return nil
 	}
 
-	chunk := streamBytes[start : end+1]
-
+	length := end - start + 1
 	ctx.Status(fiber.StatusPartialContent)
-	ctx.Set("Content-Type", contentType)
-	ctx.Set("Content-Length", fmt.Sprintf("%d", len(chunk)))
-	ctx.Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, streamSize))
-	return ctx.Send(chunk)
+	ctx.Set(fiber.HeaderContentLength, fmt.Sprintf("%d", length))
+	ctx.Set(fiber.HeaderContentRange, fmt.Sprintf("bytes %d-%d/%d", start, end, fileSize))
+
+	_, err = file.Seek(start, io.SeekStart)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Seek failed: "+err.Error())
+	}
+
+	_, err = io.CopyN(ctx.Response().BodyWriter(), file, length)
+	return err
 }
