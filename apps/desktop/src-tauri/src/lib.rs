983bc0b2ae614e25c37b8abb7193372c85f8399a
Dev-Siri <s0431668@gmail.com>
2026-01-28 00:56:33 +0530

    Get basic app shell working on tauri for dev/prod

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,16 +1,32 @@
+use tauri::tray::{TrayIcon, TrayIconBuilder};
+
+pub mod youtube;
+
+pub use youtube::*;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.handle().plugin(
+                    tauri_plugin_log::Builder::default()
+                        .level(log::LevelFilter::Info)
+                        .build(),
+                )?;
+            }
+
+            let tray_icon: TrayIcon = TrayIconBuilder::new().tooltip("Wavelength").build(app)?;
+
+            tray_icon.on_menu_event(|app_handle, event| match event.id.0.as_str() {
+                "quit" => {
+                    app_handle.exit(0);
+                }
+                _ => {}
+            });
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("Wavelength desktop launch failed.");
 }
--- apps/desktop/svelte.config.js
+++ apps/desktop/svelte.config.js
@@ -1,11 +1,14 @@
-import adapter from "@sveltejs/adapter-vercel";
+import vercelAdapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: vercelAdapter(),
+    prerender: {
+      crawl: false,
+    },
   },
 };
 
