ddb3167d8effd9f02b0462220694ab7d9e9e5432
Dev-Siri <s0431668@gmail.com>
2026-04-26 22:23:06 +0530

    Retry with https://googlecastsdk

--- apps/googlecast/vite.config.ts
+++ apps/googlecast/vite.config.ts
@@ -3,7 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
+  // base: process.env.VITE_DEV
+  //   ? undefined
+  //   : "https://wavelength-googlecast.vercel.app",
   plugins: [svelte(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
